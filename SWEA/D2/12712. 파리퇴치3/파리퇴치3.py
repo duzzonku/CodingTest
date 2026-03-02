10a8566287efd077ce3f78ff55a53ce26cc7e552
duzzonku <songkh0724@gmail.com>
2026-03-02 23:04:38 +0900

    [D2] Title: 파리퇴치3, Time: 99 ms, Memory: 59,776 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
@@ -1,36 +1,43 @@
+dr = [0, -1, 0, 1]
+dc = [1, 0, -1, 0]
+
+dr_cross = [-1, 1, 1, -1]
+dc_cross = [1, 1, -1, -1]
+
 T = int(input())
+
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    board = [list(map(int, input().split())) for _ in range(N)]
 
     max_flies = 0
-
-    dr = [0, -1, 0, 1]
-    dc = [1, 0, -1, 0]
-
-    dr_cross = [-1, -1, 1, 1]
-    dc_cross = [1, -1, -1, 1]
-
     for row in range(N):
         for col in range(N):
+            flies = board[row][col]
+            cur_r, cur_c = row, col
 
-            sum1 = arr[row][col]
             for d in range(4):
                 for k in range(1, M):
-                    nr = row + dr[d] * k
-                    nc = col + dc[d] * k
-                    if 0 <= nr < N and 0 <= nc < N:
-                        sum1 += arr[nr][nc]
+                    nr = cur_r + dr[d] * k
+                    nc = cur_c + dc[d] * k
 
-            sum2 = arr[row][col]
+                    if 0 <= nr < N and 0 <= nc < N:
+                        flies += board[nr][nc]
+
+            flies_cross = board[row][col]
+            cur_r, cur_c = row, col
+
             for d in range(4):
                 for k in range(1, M):
-                    nr = row + dr_cross[d] * k
-                    nc = col + dc_cross[d] * k
-                    if 0 <= nr < N and 0 <= nc < N:
-                        sum2 += arr[nr][nc]
+                    nr = cur_r + dr_cross[d] * k
+                    nc = cur_c + dc_cross[d] * k
 
-            if max(sum1, sum2) > max_flies:
-                max_flies = max(sum1, sum2)
+                    if 0 <= nr < N and 0 <= nc < N:
+                        flies_cross += board[nr][nc]
+
+            result = max(flies, flies_cross)
+
+            if result > max_flies:
+                max_flies = result
 
     print(f'#{tc} {max_flies}')
