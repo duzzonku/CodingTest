2dcc1d6d3e10a862f2b090318abebe5795218fa9
duzzonku <songkh0724@gmail.com>
2026-04-26 23:55:54 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 141 ms, Memory: 63,872 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,37 +1,34 @@
-from collections import deque
-
-dr = [0, -1, 0, 1]
+dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
 
-def bfs(sr, sc, er, ec):
-    queue = deque()
-    queue.append((sr, sc))
-    visited[sr][sc] = 0
+def dijkstra(r, c):
+    queue = []
+    queue.append((r, c))
+
+    dist = [[float('inf')] * N for _ in range(N)]
+    dist[r][c] = 0
 
     while queue:
-        cur_r, cur_c = queue.popleft()
+        cur_r, cur_c = queue.pop(0)
 
         for d in range(4):
             nr = cur_r + dr[d]
             nc = cur_c + dc[d]
 
             if 0 <= nr < N and 0 <= nc < N:
-                if visited[nr][nc] > visited[cur_r][cur_c] + board[nr][nc]:
-                    visited[nr][nc] = visited[cur_r][cur_c] + board[nr][nc]
+                cost = board[nr][nc] + dist[cur_r][cur_c]
+                if cost < dist[nr][nc]:
+                    dist[nr][nc] = cost
                     queue.append((nr, nc))
 
-    return visited[er][ec]
+    return dist[N - 1][N - 1]
 
 
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
     board = [list(map(int, input())) for _ in range(N)]
-    visited = [[99999999] * N for _ in range(N)]
 
-    start_r, start_c = 0, 0
-    end_r, end_c = N - 1, N - 1
-
-    result = bfs(start_r, start_c, end_r, end_c)
-    print(f'#{tc} {result}')
+    ans = dijkstra(0, 0)
+    print(f'#{tc} {ans}')
